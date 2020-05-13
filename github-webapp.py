5aefad0cacb8f773c264b59bcc0c31d21be6f254
himanshu <himanshudogra96@gmail.com>
2020-05-13 16:32:37 +0530

    webhook webapp

---
+++ github-webapp.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import json
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/github', methods=['POST'])
+def api_gh_msg():
+    if request.headers['Content-Type'] == 'application/json':
+        my_info = json.dumps(request.json)
+        print (my_info)
+        return my_info
+
+if __name__ == '__main__':
+    app.run(debug=True)
